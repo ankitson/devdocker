ccf5f40bd6931d9bb6d905790b4d951cbaad4bde
Ankit Soni <ankitsoni9@gmail.com>
2021-05-16 16:33:27 -0700

    Add rust & fselect

--- devbase.sh
+++ devbase.sh
@@ -24,9 +24,20 @@ sudo apt install -y -q \
   openssh-server       \
   net-tools            \
   unzip                \
+
+#language toolchains
+# python
+sudo apt install -y -q \
   python3              \
   python3-pip          \
   python3-venv
 
+# nodejs
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y -q nodejs
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+#tools that depend on language toolchain
+$HOME/.cargo/bin/cargo install fselect
