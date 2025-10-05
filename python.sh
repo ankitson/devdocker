242cc5cdbae9b6e579665774dd4745d028ead62d
Ankit Soni <dev@ankitson.com>
2025-10-05 16:20:30 -0700

    add torch

--- python.sh
+++ python.sh
@@ -31,6 +31,7 @@ pip3 install         \
   nose               \
   
 pip3 install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
 
 #LLM
 pip3 install         \
